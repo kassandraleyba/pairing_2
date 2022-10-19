d76eba4d97952436cda7c2bfc6a261b9099aea2d
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-18 19:29:54 -0600

    Add best friend 2

--- friends.rb
+++ friends.rb
@@ -1,7 +1,10 @@
-best_friend = "Bailey"
+best_friend = 1
+best_friend2 = "Chris"
 
 if best_friend == "Bailey"
     puts "Hi Bailey!"
+elsif best_friend2 == "Chris"
+    puts "Hi Chris!"
 else 
     puts "Hi other friend!"
 end
